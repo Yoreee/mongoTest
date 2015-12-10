1ea2056df0735246a59c31af55325e3b0619085e
Riaz Bacchus <riaz.bacchus@me.com>
2015-12-10 01:00:36 -0500

    made changes to index, newuser, and userlist views. added edit and update routes, and added editUser view.

--- routes/index.js
+++ routes/index.js
@@ -25,5 +25,50 @@ router.get('/newuser', function(req, res, next) {
 	res.render('newuser', {title: 'Add new user'});
 });
 
+router.post('/adduser', function(req, res, next) {
+	var db = req.db;
 
+	var username = req.body.username;
+	var email = req.body.email;
+
+	var collection = db.get('usercollection');
+
+	collection.insert({
+		"username": username,
+		"email": email
+	}, function(err, doc) {
+		if (err) {
+			console.log(err)
+		} else {
+			res.redirect('userlist')
+		}
+	})
+})
+
+router.get('/editUser/:id', function(req, res) {
+	var db = req.db;
+	var collection = db.get('usercollection')
+
+	collection.findById(req.params.id, function(err, doc) {
+		if (err) {
+			console.log(err)
+		} else {
+			res.render('editUser', {title: "Edit User", doc: doc})	
+		}
+	})
+})
+
+router.post('/updateUser/:id', function(req, res) {
+	var db = req.db;
+	var collection = db.get('usercollection');
+	// console.log(req.body)
+	collection.updateById(req.params.id, {username: req.body.username, email: req.body.email}, function(err) {
+		if (err) {
+			console.log(err)
+		} else {
+			console.log('update??')
+			res.redirect('/userlist')
+		}
+	})
+})
 module.exports = router;
